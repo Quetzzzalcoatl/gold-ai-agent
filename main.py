3cd8f0c726c9250d5893cc82161cd75235cb25c5
Quetzzzalcoatl <buganov1991@gmail.com>
2026-04-08 08:34:37 +0500

    add first agent logic

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
-def main() -> None:
-    print("Gold Agent started")
+from datetime import datetime
+
+
+def get_gold_price():
+    # пока заглушка (потом подключим API)
+    return 2300.0
+
+
+def predict_direction(price: float) -> str:
+    if price > 2000:
+        return "UP"
+    return "DOWN"
+
+
+def main():
+    price = get_gold_price()
+    direction = predict_direction(price)
+
+    print(f"[{datetime.now()}]")
+    print(f"Gold price: {price}")
+    print(f"Prediction: {direction}")
 
 
 if __name__ == "__main__":
